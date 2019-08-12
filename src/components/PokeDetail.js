df68564ca06057a45ff1324a504f3ec18ffa1aa5
Abdul Ghofur <abdulghoin@gmail.com>
2019-08-12 19:37:40 +0700

    catching pokemon

--- src/components/PokeDetail.js
+++ src/components/PokeDetail.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useTransition, animated, config } from "react-spring";
+import { Link } from "react-router-dom";
+
+import { Dialog, DialogContent } from "@material-ui/core";
 
 import Loader from "./Loader";
 
 import "./PokeDetail.css";
+import catchingSrc from "../assets/images/catching.gif";
 
 const changeSprites = sprites =>
   Object.values(sprites)
@@ -20,6 +24,8 @@ const PokeDetail = ({
   const [spritesLength, setSL] = useState(4);
   const [index, set] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [catchPokemon, setCatch] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -61,51 +67,135 @@ const PokeDetail = ({
 
   let { types, moves } = data;
 
+  function getCathingResult() {
+    let res = Math.floor(Math.random() * 2);
+    return res === 1 ? "success" : "failed";
+  }
+
+  function catching() {
+    setDialogOpen(true);
+    setCatch("");
+    setTimeout(() => {
+      setCatch(getCathingResult());
+    }, 2000);
+  }
+
+  function closeDialog() {
+    setDialogOpen(false);
+    setTimeout(() => {
+      setCatch("");
+    }, 500);
+  }
+
   // const src = sprites[0].src;
   return (
-    <section className="detail">
-      {transitions.map(({ item, props, key }) => (
-        <animated.div
-          key={key}
-          className="img"
-          style={{
-            ...props,
-            backgroundImage: `url(${item})`
-          }}
-        />
-      ))}
-      {/* <img src={src} alt={name} /> */}
-      <div>
-        <b className="title">Name</b>
-        <span>{name}</span>
-      </div>
-      <div className="rounded-section">
-        <b className="title">Types</b>
-        <ul>
-          {types.map(({ type: { name } }) => (
-            <li
-              key={name}
-              className="sm:w-1/5 md:w-1/3 lg:w-1/4 xl:w-1/5 sm:inline-block"
-            >
-              {name}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="rounded-section">
-        <b className="title">Moves</b>
-        <ul>
-          {moves.map(({ move: { name } }) => (
-            <li
-              key={name}
-              className="sm:w-1/5 md:w-1/3 lg:w-1/4 xl:w-1/5 sm:inline-block"
-            >
-              {name}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+    <>
+      <section className="detail">
+        {transitions.map(({ item, props, key }) => (
+          <animated.div
+            key={key}
+            className="img"
+            style={{
+              ...props,
+              backgroundImage: `url(${item})`
+            }}
+          />
+        ))}
+
+        <div className="catch">
+          <button onClick={catching}>Catch</button>
+        </div>
+        {/* <img src={src} alt={name} /> */}
+        <div>
+          <b className="title">Name</b>
+          <span>{name}</span>
+        </div>
+        <div className="rounded-section">
+          <b className="title">Types</b>
+          <ul>
+            {types.map(({ type: { name } }) => (
+              <li
+                key={name}
+                className="sm:w-1/5 md:w-1/3 lg:w-1/4 xl:w-1/5 sm:inline-block"
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="rounded-section">
+          <b className="title">Moves</b>
+          <ul>
+            {moves.map(({ move: { name } }) => (
+              <li
+                key={name}
+                className="sm:w-1/5 md:w-1/3 lg:w-1/4 xl:w-1/5 sm:inline-block"
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <Dialog
+        open={dialogOpen}
+        disableBackdropClick={true}
+        disableEscapeKeyDown={true}
+        onClose={closeDialog}
+      >
+        <div className="dialog-content">
+          {(() => {
+            switch (catchPokemon.toLowerCase()) {
+              case "success":
+                return (
+                  <div className="text-center text-xl">
+                    <b className="text-blue-400 text-3xl my-8">Success</b>
+                    <p>Yei...</p>
+                    <p>You got {name}</p>
+                    <div className="text-sm my-8">
+                      <button
+                        onClick={closeDialog}
+                        className="rounded text-white px-4 py-1  mx-2 bg-red-600 hover:bg-red-400"
+                      >
+                        Close
+                      </button>
+                      <button className="rounded text-white px-4 py-1 mx-2 bg-blue-600 hover:bg-blue-400">
+                        <Link to="/my-pokemon" onClick={closeDialog}>
+                          Check My Pokemon
+                        </Link>
+                      </button>
+                    </div>
+                  </div>
+                );
+              case "failed":
+                return (
+                  <div className="text-center text-xl">
+                    <b className="text-red-400 text-3xl my-8">Failed</b>
+                    <p>Sorry...</p>
+                    <div className="text-sm my-8">
+                      <button
+                        onClick={closeDialog}
+                        className="rounded text-white px-4 py-1  mx-2 bg-red-600 hover:bg-red-400"
+                      >
+                        Close
+                      </button>
+                      <button
+                        onClick={catching}
+                        className="rounded text-white px-4 py-1 mx-2 bg-blue-600 hover:bg-blue-400"
+                      >
+                        Try again
+                      </button>
+                    </div>
+                  </div>
+                );
+              default:
+                return <img src={catchingSrc} alt="catching" />;
+            }
+          })()}
+        </div>
+      </Dialog>
+    </>
   );
 };
 
